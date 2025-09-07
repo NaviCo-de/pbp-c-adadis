2a59558da043faeacd8951d02e843160e3fab133
NaviCo-de <aridarrell30@gmail.com>
2025-09-07 18:36:09 +0700

    Update models

--- main/models.py
+++ main/models.py
@@ -1,15 +1,27 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class Product(models.Model):
     PILIHAN_KATEGORI = [
-        'Anak-anak',
-        'Dewasa'
+        ('anak-anak', 'Anak-anak'),
+        ('dewasa', 'Dewasa')
     ]
-
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField()
-    price = models.IntegerField()
+    stock = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
     description = models.TextField()
-    thumbnail = models.URLField()
-    category = models.CharField()
-    is_featured = models.BooleanField()
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(choices=PILIHAN_KATEGORI, default='Dewasa')
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def is_product_featured(self):
+        if (self.stock > 20):
+            self.is_featured = True
+        self.is_featured = False
+        self.save()
